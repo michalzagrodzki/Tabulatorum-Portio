511bbc06005efce1ad46e1d8e6a833a5f7dafa44
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-27 12:48:35 +0100

    Add comments for code. Method is successful. Last commit to this repo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Those values are only templates to give results for functions.
 
 Story.create(
               name: { 'part_1' => 'text_1', 'part_2' => 'text_2', 'part_3' => 'text_3'}
