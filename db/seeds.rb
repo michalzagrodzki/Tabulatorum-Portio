6ca311c7ce5125356a6489dbb41af804a1a5f724
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-03-29 11:29:13 +0200

    Change structure of seeds.rb. Add new entries to seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
 
 # Those values are only templates to give results for functions.
 
-Story.create(
-              name: { 'part_1' => 'text_1', 'part_2' => 'text_2', 'part_3' => 'text_3'}
+Story.create( name: {
+                      'part_1' => 'text_1',
+                      'part_2' => 'text_2',
+                      'part_3' => 'text_3'
+                    }
              )
+
+Story.create( name: {
+                      'part_1' => 'text_4',
+                      'part_2' => 'text_5',
+                      'part_3' => 'text_6'
+                    }
+            )
+
+Story.create( name: {
+                      'part_1' => 'text_7',
+                      'part_2' => 'text_8',
+                      'part_3' => 'text_9'
+                    }
+            )
